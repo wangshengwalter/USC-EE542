d7ec7bff0cfaace4cd92eb8601f3d29987309e87
lyy885158 <125312633+lyy885158@users.noreply.github.com>
2024-09-19 19:58:48 -0700

    test

--- Lab4/client.c
+++ Lab4/client.c
@@ -5,110 +5,53 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <sys/time.h>
-#include <libgen.h>
+#include <pthread.h>
 #include <errno.h>
+#include <libgen.h>
 
-#define MAX_PACKET_SIZE 1500 // Changed to 1KB
-#define ACK_TIMEOUT_MS 40 // Changed to 40ms
-#define MAX_RETRIES 5 // Added max retries for packet loss
-#define MAX_FILENAME_SIZE 20
+#define MAX_PACKET_SIZE 8192
+#define WINDOW_SIZE 64
+#define INITIAL_TIMEOUT_MS 100
+#define MAX_FILENAME_SIZE 256
 
 typedef struct {
-    int seq_num;
-    int is_last;
-    int data_size;
+    uint32_t seq_num;
+    uint32_t ack_num;
+    uint16_t flags;
+    uint16_t checksum;
+    uint16_t data_size;
     char filename[MAX_FILENAME_SIZE];
-    char data[MAX_PACKET_SIZE - 3 * sizeof(int) - MAX_FILENAME_SIZE];
+    char data[MAX_PACKET_SIZE];
 } Packet;
 
-int create_socket() {
-    int sock = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sock < 0) {
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
-    }
-    return sock;
-}
+typedef struct {
+    Packet packets[WINDOW_SIZE];
+    int is_acked[WINDOW_SIZE];
+    int base;
+    int next_seq_num;
+    pthread_mutex_t mutex;
+    pthread_cond_t cv;
+} SlidingWindow;
 
-int send_packet(int sock, const Packet* packet, const struct sockaddr_in* server_addr) {
-    int retries = 0;
-    while (retries < MAX_RETRIES) {
-        if (sendto(sock, packet, sizeof(Packet), 0, (struct sockaddr*)server_addr, sizeof(*server_addr)) < 0) {
-            perror("sendto failed");
-            return -1;
-        }
-
-        // Wait for ACK
-        fd_set readfds;
-        struct timeval tv;
-        FD_ZERO(&readfds);
-        FD_SET(sock, &readfds);
-        tv.tv_sec = 0;
-        tv.tv_usec = ACK_TIMEOUT_MS * 1000; // Convert ms to μs
-
-        int activity = select(sock + 1, &readfds, NULL, NULL, &tv);
-        if (activity < 0) {
-            if (errno == EINTR) continue; // Interrupted system call, try again
-            perror("select error");
-            return -1;
-        } else if (activity == 0) {
-            printf("Timeout waiting for ACK, retrying packet %d (attempt %d)\n", packet->seq_num, retries + 1);
-            retries++;
-        } else {
-            int ack;
-            if (recv(sock, &ack, sizeof(ack), 0) < 0) {
-                perror("recv failed");
-                return -1;
-            }
-            if (ack == packet->seq_num) {
-                return 0; // ACK received successfully
-            }
-            printf("Received wrong ACK, retrying packet %d (attempt %d)\n", packet->seq_num, retries + 1);
-            retries++;
-        }
-    }
-    printf("Max retries reached for packet %d, giving up\n", packet->seq_num);
-    return -1;
-}
-
-void send_file(const char* filename, const char* server_ip, int server_port) {
-    int sock = create_socket();
+typedef struct {
+    int sock;
     struct sockaddr_in server_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr(server_ip);
-    server_addr.sin_port = htons(server_port);
+    SlidingWindow* window;
+    FILE* file;
+    volatile int transfer_complete;
+} ThreadArgs;
 
-    FILE* file = fopen(filename, "rb");
-    if (file == NULL) {
-        perror("Failed to open file");
-        exit(EXIT_FAILURE);
-    }
-
-    Packet packet;
-    int seq_num = 0;
-    size_t bytes_read;
-    char* base_filename = basename((char*)filename); // Get filename without path
-
-    while ((bytes_read = fread(packet.data, 1, sizeof(packet.data), file)) > 0) {
-        packet.seq_num = seq_num++;
-        packet.is_last = feof(file) ? 1 : 0;
-        packet.data_size = bytes_read;
-        strncpy(packet.filename, base_filename, MAX_FILENAME_SIZE - 1);
-        packet.filename[MAX_FILENAME_SIZE - 1] = '\0'; // Ensure null-termination
-
-        if (send_packet(sock, &packet, &server_addr) == 0) {
-            printf("Sent packet %d (%zu bytes)\n", packet.seq_num, bytes_read);
-        } else {
-            printf("Failed to send packet %d after max retries\n", packet.seq_num);
-            // You might want to implement additional error handling here
-        }
-    }
-
-    fclose(file);
-    close(sock);
-    printf("File transfer complete\n");
-}
+// Function prototypes
+int create_socket();
+void setup_server_addr(struct sockaddr_in* server_addr, const char* server_ip, int server_port);
+void init_sliding_window(SlidingWindow* window);
+void* send_packets_thread(void* arg);
+void* receive_acks_thread(void* arg);
+void send_packet(int sock, const Packet* packet, const struct sockaddr_in* server_addr);
+uint16_t calculate_checksum(const Packet* packet);
+void fill_window_from_file(SlidingWindow* window, FILE* file, const char* filename);
+void wait_for_window_space(SlidingWindow* window);
+void send_fin_packet(int sock, const struct sockaddr_in* server_addr);
 
 int main(int argc, char* argv[]) {
     if (argc != 4) {
@@ -120,7 +63,162 @@ int main(int argc, char* argv[]) {
     const char* server_ip = argv[2];
     int server_port = atoi(argv[3]);
 
-    send_file(filename, server_ip, server_port);
+    int sock = create_socket();
+    struct sockaddr_in server_addr;
+    setup_server_addr(&server_addr, server_ip, server_port);
+
+    FILE* file = fopen(filename, "rb");
+    if (file == NULL) {
+        perror("Failed to open file");
+        exit(EXIT_FAILURE);
+    }
+
+    SlidingWindow window;
+    init_sliding_window(&window);
+
+    ThreadArgs args = {
+        .sock = sock,
+        .server_addr = server_addr,
+        .window = &window,
+        .file = file,
+        .transfer_complete = 0
+    };
+
+    pthread_t send_thread, ack_thread;
+    pthread_create(&send_thread, NULL, send_packets_thread, &args);
+    pthread_create(&ack_thread, NULL, receive_acks_thread, &args);
+
+    char* base_filename = basename((char*)filename);
+    while (!args.transfer_complete) {
+        fill_window_from_file(&window, file, base_filename);
+        wait_for_window_space(&window);
+    }
+
+    send_fin_packet(sock, &server_addr);
+
+    pthread_join(send_thread, NULL);
+    pthread_join(ack_thread, NULL);
+
+    fclose(file);
+    close(sock);
+    printf("File transfer complete\n");
 
     return 0;
+}
+
+int create_socket() {
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock < 0) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+    return sock;
+}
+
+void setup_server_addr(struct sockaddr_in* server_addr, const char* server_ip, int server_port) {
+    memset(server_addr, 0, sizeof(*server_addr));
+    server_addr->sin_family = AF_INET;
+    server_addr->sin_addr.s_addr = inet_addr(server_ip);
+    server_addr->sin_port = htons(server_port);
+}
+
+void init_sliding_window(SlidingWindow* window) {
+    window->base = 0;
+    window->next_seq_num = 0;
+    memset(window->is_acked, 0, sizeof(window->is_acked));
+    pthread_mutex_init(&window->mutex, NULL);
+    pthread_cond_init(&window->cv, NULL);
+}
+
+void* send_packets_thread(void* arg) {
+    ThreadArgs* args = (ThreadArgs*)arg;
+    while (!args->transfer_complete) {
+        pthread_mutex_lock(&args->window->mutex);
+        while (args->window->next_seq_num < args->window->base + WINDOW_SIZE &&
+               !args->window->is_acked[args->window->next_seq_num % WINDOW_SIZE]) {
+            send_packet(args->sock, &args->window->packets[args->window->next_seq_num % WINDOW_SIZE], &args->server_addr);
+            args->window->next_seq_num++;
+        }
+        pthread_mutex_unlock(&args->window->mutex);
+        usleep(1000); // Sleep for 1ms to avoid busy waiting
+    }
+    return NULL;
+}
+
+void* receive_acks_thread(void* arg) {
+    ThreadArgs* args = (ThreadArgs*)arg;
+    Packet ack_packet;
+    struct sockaddr_in server_addr;
+    socklen_t addr_len = sizeof(server_addr);
+
+    while (!args->transfer_complete) {
+        ssize_t received = recvfrom(args->sock, &ack_packet, sizeof(Packet), 0,
+                                    (struct sockaddr*)&server_addr, &addr_len);
+        if (received < 0) {
+            if (errno != EAGAIN && errno != EWOULDBLOCK) {
+                perror("recvfrom failed");
+            }
+            continue;
+        }
+
+        if (ack_packet.flags & 0x0010) { // ACK flag
+            pthread_mutex_lock(&args->window->mutex);
+            for (int i = args->window->base; i <= ack_packet.ack_num; i++) {
+                args->window->is_acked[i % WINDOW_SIZE] = 1;
+            }
+            args->window->base = ack_packet.ack_num + 1;
+            pthread_cond_signal(&args->window->cv);
+            pthread_mutex_unlock(&args->window->mutex);
+        }
+
+        if (ack_packet.flags & 0x0001) { // FIN flag
+            args->transfer_complete = 1;
+        }
+    }
+    return NULL;
+}
+
+void send_packet(int sock, const Packet* packet, const struct sockaddr_in* server_addr) {
+    if (sendto(sock, packet, sizeof(Packet), 0,
+               (struct sockaddr*)server_addr, sizeof(*server_addr)) < 0) {
+        perror("sendto failed");
+    }
+}
+
+uint16_t calculate_checksum(const Packet* packet) {
+    // Implement a proper checksum algorithm here
+    return 0;
+}
+
+void fill_window_from_file(SlidingWindow* window, FILE* file, const char* filename) {
+    pthread_mutex_lock(&window->mutex);
+    while (window->next_seq_num < window->base + WINDOW_SIZE) {
+        Packet* packet = &window->packets[window->next_seq_num % WINDOW_SIZE];
+        packet->seq_num = window->next_seq_num;
+        packet->data_size = fread(packet->data, 1, MAX_PACKET_SIZE, file);
+        if (packet->data_size == 0) {
+            break; // End of file
+        }
+        packet->flags = 0x0000; // No flags set
+        strncpy(packet->filename, filename, MAX_FILENAME_SIZE - 1);
+        packet->filename[MAX_FILENAME_SIZE - 1] = '\0';
+        packet->checksum = calculate_checksum(packet);
+        window->next_seq_num++;
+    }
+    pthread_mutex_unlock(&window->mutex);
+}
+
+void wait_for_window_space(SlidingWindow* window) {
+    pthread_mutex_lock(&window->mutex);
+    while (window->next_seq_num >= window->base + WINDOW_SIZE) {
+        pthread_cond_wait(&window->cv, &window->mutex);
+    }
+    pthread_mutex_unlock(&window->mutex);
+}
+
+void send_fin_packet(int sock, const struct sockaddr_in* server_addr) {
+    Packet fin_packet = {0};
+    fin_packet.flags = 0x0001; // FIN flag
+    fin_packet.checksum = calculate_checksum(&fin_packet);
+    send_packet(sock, &fin_packet, server_addr);
 }
