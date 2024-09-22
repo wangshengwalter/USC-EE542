4991628af63237450d8de5d7246548ab3ec333b0
wangshengwalter <wangshengwalter@gmail.com>
2024-09-21 17:42:45 -0700

    Update clientp.c

--- Lab4/clientp.c
+++ Lab4/clientp.c
@@ -102,7 +102,7 @@ private:
         } else if (activity == 0) {
             auto end = std::chrono::high_resolution_clock::now();
             std::chrono::duration<double> elapsed = end - start;
-            printf("Elapsed time: %f\n", elapsed.count());
+            printf("Elapsed time: %fms\n", elapsed_time*1000);
             return -1;  // Timeout
         } else {
             int ack;
