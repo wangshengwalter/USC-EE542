801464829a79cb48cb30f4f6314187df5b91fd39
wangshengwalter <wangshengwalter@gmail.com>
2024-09-25 04:08:31 -0700

    Update clients.c

--- Lab4/clients.c
+++ Lab4/clients.c
@@ -356,12 +356,18 @@ int main(int argc, char* argv[]) {
 
     // sender.combine_files(filename, file_separator);
     //create the sliding window for each file part
+    std::vector<std::thread> threads;
     for (int i = 0; i < fileNames.size(); i++) {
-        std::thread client([&]() {
+        threads.emplace_back([&, i]() { // Pass 'i' by value to avoid issues with capturing
             SlidingWindowClient client(fileNames[i].c_str(), ip, port, DEFAULT_WINDOW_SIZE, DEFAULT_TIMEOUT);
             client.run();
         });
-        client.join();
+    }
+    // Join all threads after starting them
+    for (auto& thread : threads) {
+        if (thread.joinable()) {
+            thread.join();
+        }
     }
 
     auto end = std::chrono::high_resolution_clock::now();
